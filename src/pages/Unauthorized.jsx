eecb33e337640df60f7a7caa8c1d7707d7a716bf
anantharjun672001-a11y <anantharjun672001@gmail.com>
2026-04-25 13:53:49 +0530

    unauthorized  updated

--- src/pages/Unauthorized.jsx
+++ src/pages/Unauthorized.jsx
@@ -4,7 +4,7 @@ const Unauthorized = () => {
     return (
         <div>
             <h1 className="text-3xl font-bold text-center text-white mb-2">
-                403 - Unauthorized
+                403 - Unauthorized Access
             </h1>
             <p className="text-center text-gray-200 mb-6">
                 You do not have permission to access this page.
